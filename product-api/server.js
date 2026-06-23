aaae38c9cac8abc72b7b69ba04e262f95949f755
artiverma-00 <vermaarti930@gmail.com>
2026-06-24 00:17:02 +0530

    up0.4

--- product-api/server.js
+++ product-api/server.js
@@ -3,41 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-function normalizeOrigin(value) {
-  const trimmed = String(value || "").trim();
-
-  if (!trimmed) {
-    return "";
-  }
-
-  try {
-    return new URL(trimmed).origin;
-  } catch (error) {
-    return trimmed.replace(/\/$/, "");
-  }
-}
-
-const defaultAllowedOrigins = [
-  "http://localhost:5173",
-  "https://product-api-one-gules.vercel.app",
-];
-
-const allowedOrigins = new Set(
-  [process.env.FRONTEND_ORIGIN, ...defaultAllowedOrigins]
-    .flatMap((value) => String(value || "").split(","))
-    .map(normalizeOrigin)
-    .filter(Boolean),
-);
-
 app.use(express.json());
 
 app.use((req, res, next) => {
-  const requestOrigin = req.headers.origin;
-
-  if (requestOrigin && allowedOrigins.has(requestOrigin)) {
-    res.setHeader("Access-Control-Allow-Origin", requestOrigin);
-    res.setHeader("Vary", "Origin");
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader(
     "Access-Control-Allow-Methods",
